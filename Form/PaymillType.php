9728c378180340eff587f405609a77e1359cfbdb
Paulo Rodrigues Pinto <regularjack@gmail.com>
2014-07-09 19:16:45 +0100

    Enforce presence of the token when handling form submission

--- Form/PaymillType.php
+++ Form/PaymillType.php
@@ -2,19 +2,34 @@
 
 namespace Memeoirs\PaymillBundle\Form;
 
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\AbstractType,
+    Symfony\Component\Form\FormBuilderInterface,
+    Symfony\Component\Validator\Constraints\NotBlank;
 
 class PaymillType extends AbstractType
 {
-    public function buildForm (FormBuilderInterface $builder, array $options)
+    private $translator;
+
+    public function __construct($translator)
     {
+        $this->translator = $translator;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $tokenNotBlank = new NotBlank(array(
+            'message' => $this->translator->trans('default', array(), 'errors')
+        ));
+
         $builder
             ->add('number' , 'text',   array('required' => false, 'label' => 'Card number'))
             ->add('expiry' , 'text',   array('required' => false, 'label' => 'Expires'))
             ->add('holder' , 'text',   array('required' => false, 'label' => 'Name on card'))
             ->add('cvc'    , 'text',   array('required' => false, 'label' => 'Card code'))
-            ->add('token'  , 'hidden', array('required' => false))
+            ->add('token'  , 'hidden', array(
+                'required' => false,
+                'constraints' => array($tokenNotBlank)
+            ))
         ;
     }
 
